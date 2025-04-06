145de2c266f9f252ed76f80a118d5966070963c4
SY-42 <103295003+SY-42@users.noreply.github.com>
2025-04-06 13:41:00 -0700

    Add dataframe processing

--- cult-classic-backend/main.py
+++ cult-classic-backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
+import pandas as pd
 
 app = FastAPI(title="Cult Classic Calculator API")
 
@@ -22,6 +23,26 @@ class MovieData(BaseModel):
     tagline: str
     description: str
     genre: str
+    revenue: float
+    budget: float
+    adult: bool
+
+    def get_df(self):
+        genre_list = ['Action', 'Science Fiction', 'Adventure', 'Drama', 'Crime',
+       'Thriller', 'Fantasy', 'Comedy', 'Romance', 'Western', 'Mystery',
+       'War', 'Animation', 'Family', 'Horror', 'Music', 'History',
+       'Documentary', 'TV Movie', 'Genreless']
+        df = pd.DataFrame(columns=['release_year', 'runtime', 'revenue', 'adult', 'budget'] + genre_list)
+        df.loc[0] = [
+            self.year,
+            self.runtime,
+            self.revenue,
+            int(self.adult),
+            self.budget
+        ]+[0 if genre != self.genre else 1 for genre in genre_list]
+
+        return df
+
 
 @app.get("/")
 async def root():
@@ -31,8 +52,11 @@ async def root():
 @app.post("/calculate")
 async def calculate_cult_probability(movie: MovieData):
     # TODO: implement cult classic probability calculation logic
-    return {
 
+
+
+    return {
+        
     }
 
 if __name__ == "__main__":
